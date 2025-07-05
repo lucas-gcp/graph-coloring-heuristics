3df76e2f2223ce9ae1c8e6d5ccef1cc1bd47d5fb
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2025-07-04 21:15:31 -0300

    feat: implement struct 'VertexDegree'

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
 #include "graph.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,17 +12,25 @@ Graph::Graph(ifstream &in_file) {
     string line, buffer;
     do {
         getline(in_file, line);
-    } while(line[0] != 'p');
-    
+    } while (line[0] != 'p');
+
     stringstream info(line);
-    
+
     info >> buffer >> buffer >> n_vertices >> n_edges;
-    cout << n_vertices << " " << n_edges << endl;;
-    avg_d = n_edges/n_vertices;
+    cout << n_vertices << " " << n_edges << endl;
+    avg_d = n_edges / n_vertices;
 
     neighbors.assign(n_vertices, vector<unsigned>());
-    degrees.assign(n_vertices, 0);
-    
+    degrees.reserve(n_vertices);
+
+    for (unsigned i = 0; i < n_vertices; i++) {
+        VertexDegree vertexDegree;
+        vertexDegree.index = i;
+        vertexDegree.degree = 0;
+
+        degrees.push_back(vertexDegree);
+    }
+
     for (unsigned i = 0; i < n_vertices; i++)
         neighbors[i].reserve(avg_d);
 
@@ -29,9 +38,9 @@ Graph::Graph(ifstream &in_file) {
     unsigned u, v;
     for (unsigned i = 0; i < n_edges; i++) {
         in_file >> e >> u >> v;
-        neighbors[u-1].push_back(v-1);
-        neighbors[v-1].push_back(u-1);
-        degrees[u-1]++;
-        degrees[v-1]++;
+        neighbors[u - 1].push_back(v - 1);
+        neighbors[v - 1].push_back(u - 1);
+        degrees[u - 1].degree++;
+        degrees[v - 1].degree++;
     }
 }
--- src/graph.h
+++ src/graph.h
@@ -6,19 +6,23 @@
 
 using namespace std;
 
+struct {
+    unsigned index;
+    unsigned degree;
+} typedef VertexDegree;
+
 class Graph {
-    private:
-        // unsigned n_vertices, n_edges;
-        unsigned avg_d;
+   private:
+    // unsigned n_vertices, n_edges;
+    unsigned avg_d;
 
-    public:
-        Graph(ifstream &in_file);
-        
-        unsigned n_vertices, n_edges;
-        vector<unsigned> degrees;
-        vector<vector<unsigned>> neighbors;
-        // vector<int> &get_neighbors(int v);
+   public:
+    Graph(ifstream &in_file);
 
+    unsigned n_vertices, n_edges;
+    vector<VertexDegree> degrees;
+    vector<vector<unsigned>> neighbors;
+    // vector<int> &get_neighbors(int v);
 };
 
 #endif
